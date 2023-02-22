03ca739517f653f8576b59677debf1145fc93250
LordPelmeha <sashasofolov@gmail.com>
2023-02-22 20:07:52 +0300

    Added a second task and tests to it

--- Homework2/tasks.cs
+++ Homework2/tasks.cs
@@ -1,15 +1,21 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-
 namespace Homework2
 {
     public class Program
     {
         static void Main()
         {
-            foreach (var x in MasFilter(new int[] {1,2,3,4,4},x=>x%2==0))
-                Console.Write(x+" ");
+            Console.WriteLine("Задание 1");
+            foreach (var x in MasFilter(new int[] { 1, 2, 3, 4, 4 }, x => x % 2 == 0))
+                Console.Write(x + " ");
+            Console.WriteLine();
+            Console.WriteLine("Задание 2");
+            int[] SecondArr = new int[] { 1, 2, 3, 4, 5, 6 };
+            SwapParts(ref SecondArr);
+            foreach (var x in SecondArr)
+                Console.Write(x + " ");
         }
-        public static int[] MasFilter(int[] s,Func<int,bool> f)
+        public static int[] MasFilter(int[] s, Func<int, bool> f)
         {
             int[] a = new int[s.Count()];
             int count = 0;
@@ -20,6 +26,11 @@ namespace Homework2
                     count++;
                 }
             return a[..count];
+        }
+        public static void SwapParts(ref int[] s)
+        {
+            if (s.Length % 2 != 0) throw new ArgumentException("Длина массива должна быть чётной!");
+            s = s[(s.Length / 2)..].Concat(s[..(s.Length / 2)]).ToArray();
         }
     }
 }
--- HomeworkTests/UnitTest1.cs
+++ HomeworkTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Internal;
 using static Homework2.Program;
 namespace HomeworkTests
 
@@ -9,7 +10,17 @@ namespace HomeworkTests
         {
             Assert.That(MasFilter(new int[] { 2, 4 }, x => x % 2 == 1), Is.EqualTo(new int[] { }));
             Assert.That(MasFilter(new int[] { }, x => x < 0), Is.EqualTo(new int[] { }));
-            Assert.That(MasFilter(new int[] { 1, 3,2,-2,4,0,5 }, x => x % 2 == 0), Is.EqualTo(new int[] {2,-2,4,0 }));
+            Assert.That(MasFilter(new int[] { 1, 3, 2, -2, 4, 0, 5 }, x => x % 2 == 0), Is.EqualTo(new int[] { 2, -2, 4, 0 }));
+        }
+        [Test]
+        public void TestSwapParts()
+        {
+            int[] s1 = new int[] { 1, 2, 3, 4 };
+            SwapParts(ref s1);
+            Assert.That(s1, Is.EqualTo(new int[] { 3, 4, 1, 2 }));
+            int[] s2 = new int[] { };
+            SwapParts(ref s2);
+            Assert.That(s2, Is.EqualTo(new int[] { }));
         }
     }
 }
