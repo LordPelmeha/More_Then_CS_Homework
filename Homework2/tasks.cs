ae46c2c9aa6dfe8f3a76f000257f32c75a38d6e5
LordPelmeha <sashasofolov@gmail.com>
2023-02-22 19:28:54 +0300

    Added a first task and tests to it

--- Homework2/tasks.cs
+++ Homework2/tasks.cs
@@ -1,17 +1,25 @@
-﻿namespace Homework2
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Homework2
 {
     public class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            foreach (var x in MasFilter(new int[] {1,2,3,4,4},x=>x%2==0))
+                Console.Write(x+" ");
         }
-
-        public static double ProductAtoB(int x, int y)
+        public static int[] MasFilter(int[] s,Func<int,bool> f)
         {
-            double prod = 1;
-            for (Int64 i = Math.Min(x, y) + Math.Min(x, y) % 2; i <= Math.Max(x, y) - Math.Max(x, y) % 2; i += 2) prod *= i;
-            return prod;
+            int[] a = new int[s.Count()];
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+                if (f(s[i]))
+                {
+                    a[count] = s[i];
+                    count++;
+                }
+            return a[..count];
         }
     }
 }
--- HomeworkTests/UnitTest1.cs
+++ HomeworkTests/UnitTest1.cs
@@ -1,9 +1,15 @@
 using static Homework2.Program;
 namespace HomeworkTests
-    
+
 {
     public class Tests
     {
-        
+        [Test]
+        public void TestMasFilter()
+        {
+            Assert.That(MasFilter(new int[] { 2, 4 }, x => x % 2 == 1), Is.EqualTo(new int[] { }));
+            Assert.That(MasFilter(new int[] { }, x => x < 0), Is.EqualTo(new int[] { }));
+            Assert.That(MasFilter(new int[] { 1, 3,2,-2,4,0,5 }, x => x % 2 == 0), Is.EqualTo(new int[] {2,-2,4,0 }));
+        }
     }
 }
