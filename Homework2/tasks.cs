d7223a92e08123b62e3db1273e09b2b1736be465
LordPelmeha <sashasofolov@gmail.com>
2023-02-22 18:13:44 +0300

    Added Homework2 and UnitTest

---
+++ Homework2/tasks.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework2
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static double ProductAtoB(int x, int y)
+        {
+            double prod = 1;
+            for (Int64 i = Math.Min(x, y) + Math.Min(x, y) % 2; i <= Math.Max(x, y) - Math.Max(x, y) % 2; i += 2) prod *= i;
+            return prod;
+        }
+    }
+}
---
+++ HomeworkTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+using static Homework2.Program;
+namespace HomeworkTests
+    
+{
+    public class Tests
+    {
+        
+    }
+}
