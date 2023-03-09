f1c7ca59d7d17c624b45eceeacf9bd0e0ecc4afc
LordPelmeha <sashasofolov@gmail.com>
2023-03-09 14:31:40 +0300

    Added second task

--- Homework3/tasks.cs
+++ Homework3/tasks.cs
@@ -12,6 +12,7 @@ namespace Homework3
                 Console.Write(x + " ");
             Console.WriteLine();
             Console.WriteLine("задание 2");
+            Console.WriteLine(StarToItalic(Console.ReadLine()));
             Console.WriteLine();
             Console.WriteLine("задание 3");
             Console.WriteLine();
@@ -23,5 +24,6 @@ namespace Homework3
         /// Находит в строке все автомобильные номера и возвращает массив из них.
         /// </summary>
         static string[] RusCarNum(string s) => Regex.Matches(s, @"\b[АВЕКМНОРСТУХ]{1}[0-9]{3}[АВЕКМНОРСТУХ]{2}[0-9]{2}\b").Select(x => x.ToString()).ToArray();
+        static string StarToItalic(string s) => Regex.Replace(s, @"((?<!\*)[*])([\w\s]+)([*](?!\*))", @"<em>$2</em>").ToString();
     }
 }
