351efdf805675bf4e3365a1502e3afca8b0889f5
LordPelmeha <sashasofolov@gmail.com>
2023-03-29 20:17:39 +0300

    Added first and second tasks

---
+++ Homework5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.Json.Serialization.Metadata;
+using System.Text.RegularExpressions;
+
+namespace Homework5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("задание 1");
+            FormRandomBinaryFile("task1.dat");
+            Console.WriteLine();
+            Console.WriteLine("задание 2");
+            Console.WriteLine(ReturnNumFromFile("input-files/task2.txt", 5));
+
+
+        }
+        /// <summary>
+        /// Формирует бинарный файл из N случайных вещественных чисел в диапазоне от a до b (a ≤ b)
+        /// </summary>
+
+        static void FormRandomBinaryFile(string path, int n=20, int a=-50, int b=50)
+        {
+            if (n < 0) throw new ArgumentException("Число элементов должно быть неотрицательным!");
+            if (a > b) throw new ArgumentException("Левая граница не может быть больше правой!");
+            if (path == null || path == "") throw new ArgumentNullException("Файл не должен быть пустым!");
+            var r = new Random();
+            using (var bw = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8, false))
+            {
+                for (int i = 0; i < n; i++)
+                    bw.Write(r.Next(a, b + 1) * r.NextDouble());
+            }
+        }
+        /// <summary>
+        /// Возвращает K-й элемент файла (элементы нумеруются с 0). Если такой элемент отсутствует, возвращает −1.
+        /// </summary>
+        static int ReturnNumFromFile(string path,int k)
+        {
+            var s = Regex.Split(File.ReadAllText(path), @"\s").Select(x=>int.Parse(x)).ToArray();
+            if (k >= s.Length|| k<1) return -1;
+            else return s[k];
+        }
+    }
+}
