3c23371e1e5999605a32955c3702ebbc2b5adea7
LordPelmeha <sashasofolov@gmail.com>
2023-03-30 15:01:22 +0300

    Added third task

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -15,14 +15,15 @@ namespace Homework5
             Console.WriteLine();
             Console.WriteLine("задание 2");
             Console.WriteLine(ReturnNumFromFile("input-files/task2.txt", 5));
-
-
+            Console.WriteLine();
+            Console.WriteLine("задание 3");
+            Console.WriteLine(GetTrackAndArtistName("input-files/ответы на контрольную.mp3"));
         }
         /// <summary>
         /// Формирует бинарный файл из N случайных вещественных чисел в диапазоне от a до b (a ≤ b)
         /// </summary>
 
-        static void FormRandomBinaryFile(string path, int n=20, int a=-50, int b=50)
+        static void FormRandomBinaryFile(string path, int n = 20, int a = -50, int b = 50)
         {
             if (n < 0) throw new ArgumentException("Число элементов должно быть неотрицательным!");
             if (a > b) throw new ArgumentException("Левая граница не может быть больше правой!");
@@ -37,11 +38,19 @@ namespace Homework5
         /// <summary>
         /// Возвращает K-й элемент файла (элементы нумеруются с 0). Если такой элемент отсутствует, возвращает −1.
         /// </summary>
-        static int ReturnNumFromFile(string path,int k)
+        static int ReturnNumFromFile(string path, int k)
         {
-            var s = Regex.Split(File.ReadAllText(path), @"\s").Select(x=>int.Parse(x)).ToArray();
-            if (k >= s.Length|| k<1) return -1;
+            var s = Regex.Split(File.ReadAllText(path), @"\s").Select(x => int.Parse(x)).ToArray();
+            if (k >= s.Length || k < 1) return -1;
             else return s[k];
+        }
+        /// <summary>
+        /// Извлекает название трека, исполнителя и альбома из рикрола
+        /// </summary>
+        static string GetTrackAndArtistName(string path)
+        {
+            var a = File.ReadAllText(path);
+            return $"{a[(a.Length - 125)..(a.Length - 101)]}-{a[(a.Length - 96)..(a.Length - 84)]}\nАыльбом - {a[(a.Length - 65)..(a.Length - 50)]}";
         }
     }
 }
