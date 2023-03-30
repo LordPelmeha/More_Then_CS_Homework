ceed7680ab38348f3dc8535cbef3731730282a74
LordPelmeha <sashasofolov@gmail.com>
2023-03-30 20:39:25 +0300

    Added CountWordsInFile and GetListFromFile

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Diagnostics.SymbolStore;
+using System.IO;
 using System.Text;
 using System.Text.Json.Serialization.Metadata;
 using System.Text.RegularExpressions;
@@ -18,6 +19,14 @@ namespace Homework5
             Console.WriteLine();
             Console.WriteLine("задание 3");
             Console.WriteLine(GetTrackAndArtistName("input-files/ответы на контрольную.mp3"));
+            Console.WriteLine();
+            Console.WriteLine("задание 5");
+            Console.WriteLine(CountWordsInFile("input-files/task5.txt"));
+            Console.WriteLine();
+            Console.WriteLine("задание 6");
+            // Console.WriteLine(GetListFromFile("input-files/IMDB.csv").Count());
+            foreach (var movie in GetListFromFile("input-files/IMDB.csv"))
+                Console.WriteLine($"{movie.Title} {movie.OriginalLanguage} {movie.VoteCount} {movie.VoteAverage}");
         }
         /// <summary>
         /// Формирует бинарный файл из N случайных вещественных чисел в диапазоне от a до b (a ≤ b)
@@ -40,7 +49,7 @@ namespace Homework5
         /// </summary>
         static int ReturnNumFromFile(string path, int k)
         {
-            var s = Regex.Split(File.ReadAllText(path), @"\s").Select(x => int.Parse(x)).ToArray();
+            var s = Regex.Split(File.ReadAllText(path), @"\s+").Select(x => int.Parse(x)).ToArray();
             if (k >= s.Length || k < 1) return -1;
             else return s[k];
         }
@@ -50,7 +59,58 @@ namespace Homework5
         static string GetTrackAndArtistName(string path)
         {
             var a = File.ReadAllText(path);
-            return $"{a[(a.Length - 125)..(a.Length - 101)]}-{a[(a.Length - 96)..(a.Length - 84)]}\nАыльбом - {a[(a.Length - 65)..(a.Length - 50)]}";
+            return $"{a[(a.Length - 125)..(a.Length - 101)]}-{a[(a.Length - 96)..(a.Length - 84)]}\nАыльбом - {a[(a.Length - 65)..(a.Length - 35)]}";
+        }
+        /// <summary>
+        /// Считает количество слов в файле, разделённых пробелами или символами переноса строки
+        /// </summary>
+        static int CountWordsInFile(string path)
+        {
+            return Regex.Split(File.ReadAllText(path), @"\s+").Length;
+        }
+        /// <summary>
+        /// Возвращает отсортированный по рейтингу список фильмов, у которых количество отзывов не менее 100.
+        /// </summary>
+        static List<Movie> GetListFromFile(string path)
+        {
+            var ar = File.ReadAllLines(path);
+            List<Movie> MoviesList = new List<Movie>();
+            foreach (var x in ar[1..])
+            {
+                var movie = new Movie(x);
+                if (movie.VoteCount >= 100)
+                    MoviesList.Add(movie);
+                //Console.WriteLine($"{movie.Title} {movie.OriginalLanguage} {movie.VoteCount} {movie.VoteAverage}");
+            }
+            for (int i = 0; i < MoviesList.Count(); i++)
+            {
+                for (int j = 0; j < MoviesList.Count(); j++)
+                {
+                    if (MoviesList[i].VoteAverage < MoviesList[j].VoteAverage)
+                        (MoviesList[i], MoviesList[j]) = (MoviesList[j], MoviesList[i]);
+                }
+            }
+            return MoviesList;
+        }
+    }
+    public class Movie
+    {
+        public string Title { get; private set; }
+        public string OriginalLanguage { get; private set; }
+        public int VoteCount { get; private set; }
+        public double VoteAverage { get; private set; }
+        public Movie(string s)
+        {
+            if (Regex.Split(s, @",").Length >= 5)
+            {
+                var ar = Regex.Match(s, @"((?<=,).*?(?=,\w{2},\d+,\d{1})),((?<=,)\w{2}(?=,)),(\d+),(\d\.?\d?)");
+
+                Title = ar.Groups[1].ToString();
+                OriginalLanguage = ar.Groups[2].ToString();
+                //Console.WriteLine(ar[3] + " " + ar[4]);
+                VoteCount = int.Parse(ar.Groups[3].ToString());
+                VoteAverage = double.Parse(Regex.Replace(ar.Groups[4].ToString(), @"\.", @","));
+            }
         }
     }
 }
