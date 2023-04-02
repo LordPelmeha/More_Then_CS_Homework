19421e71fc30b1de871bd232beeeb372e73963a8
LordPelmeha <sashasofolov@gmail.com>
2023-04-02 16:58:07 +0300

    Added ATM class

--- HomeworkATM/ATM.cs
+++ HomeworkATM/ATM.cs
@@ -8,5 +8,32 @@ namespace HomeworkATM
 {
     public class ATM
     {
+        public long ID;
+        private string bank;
+        private Dictionary<string, int> Cassette;
+        private List<string> History;
+        private string Key;
+        public string Bank
+        {
+            get
+            {
+                return bank;
+            }
+            private set
+            {
+                if (value != null && value != "")
+                    bank = value;
+            }
+
+        }
+        public ATM(string Bank)
+        {
+            var r = new Random();
+            ID = r.NextInt64(10000000, 100000000);
+            this.Bank = Bank;
+            Cassette = new Dictionary<string, int>();
+            History = new List<string>();
+            Key = "ILoveMMCS";
+        }
     }
 }
